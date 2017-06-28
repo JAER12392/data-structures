b29d2a1a888a0e1d9c13cb3eda988cdb5e5c3a29
Hack Reactor Student <student@hackreactor.com>
2017-06-28 12:07:16 -0500

    Passed Stack(functional-shared)

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,37 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.counter = 0;
+  extend(someInstance, stackMethods);
+
+  return someInstance;
+};
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
 };
 
 var stackMethods = {};
 
+stackMethods.size = function() {
+  if (this.counter < 0) {
+    this.counter = 0;
+  }  
+  return this.counter;
+};
+
+stackMethods.push = function(value) {
+  stackMethods[this.counter] = value;
+  this.counter++;
+};
+
+stackMethods.pop = function() {
+  if (this.counter > 0) {
+    this.counter--;
+  }
+  return stackMethods[this.counter];
+};
 
