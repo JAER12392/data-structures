b8aa8faa293279ca690210af42f6ccb511ef7132
Hack Reactor Student <student@hackreactor.com>
2017-06-30 10:22:34 -0500

    Finished Linked List

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,29 +2,45 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
-  list.node = null;
+  
   
   list.addToTail = function(value) {
-    list.node = Node(value);    
+// declared node 
+    var node = Node(value);
+// updating the list head to be the value we've passed in
     if (list.head === null) {
-      list.head = list.node;
-      
-    } 
-    list.head.next = list.node;
-    // list.head.next = list.tail;
-    // list.tail = list.tail.next;
-// node.next = pointer!
+// and setting both the tail and the head to be the value we're passing in to aTT
+      list.head = node;
+      list.tail = node; 
+// so now we've dealt with the case of starting off the linked list - right now,
+// we've set the single node in our list to be both the head and the tail.
+    } else {
+// now if the function is run again, we're taking the tail node, and setting it to 
+// point (using next property) to the newly added node, which is now the tail.
+      list.tail.next = node;
+      list.tail = node;
+    }
   };
 
   list.removeHead = function() {
-    var orginal = list.head.value;
-    list.head.value = list.tail.value;
-    return orginal;
-    
+    var head = list.head.value;
+    list.head = list.head.next;
+    return head;
   };
 
   list.contains = function(target) {
   
+    var lookThroughLinkedList = function(node) {
+      if (node.value === target) {
+        return true;
+      } else if (node.next === null) {
+        return false;
+      } else {
+        return lookThroughLinkedList(node.next);
+      }
+    };
+    return lookThroughLinkedList(list.head);
+    // return lookThroughLinkedList(list.head);
   };
 
   return list;
